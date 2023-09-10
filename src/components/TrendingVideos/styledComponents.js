7a5d6f6e52af25f9a14e4b34fb8ce28ab607b46e
dhanushg <dhanushg942000@gmail.com>
2023-09-10 10:04:12 +0000

    solution

---
+++ src/components/TrendingVideos/styledComponents.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+
+export const TrendingContainer = styled.div`
+  background-color: ${props => props.bgColor};
+  min-height: 100vh;
+  margin-top: 60px;
+  margin-bottom: 60px;
+  overflow-y: auto;
+  width: 100%;
+  @media screen and (min-width: 768px) {
+    padding-left: 25px;
+  }
+`
+export const TrendingVideoTitle = styled.div`
+  display: flex;
+  align-items: center;
+`
+export const TrendingTitleIconContainer = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50px;
+  margin-left: 20px;
+  margin-right: 10px;
+  display: flex;
+  justify-content: center;
+  justify-items: center;
+`
+export const TrendingText = styled.h1`
+  font-family: 'Roboto';
+  font-size: 25px;
+  color: ${props => props.color};
+`
+export const TrendingVideoList = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0px;
+`
+export const LoaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`
